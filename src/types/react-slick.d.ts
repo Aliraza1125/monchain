5cf384659cf96ea1c47cece31410805f45b620e7
Ali Raza <alirazaasif2511@gmail.com>
2024-12-18 21:22:15 +0600

    updated

--- src/types/react-slick.d.ts
+++ src/types/react-slick.d.ts
@@ -1,5 +1,66 @@
 declare module 'react-slick' {
-    const Slider: any;
-    export default Slider;
+  import { ComponentType } from 'react';
+
+  export interface Settings {
+    accessibility?: boolean;
+    adaptiveHeight?: boolean;
+    afterChange?: (currentSlide: number) => void;
+    appendDots?: (dots: React.ReactNode) => React.ReactNode;
+    arrows?: boolean;
+    asNavFor?: Slider;
+    autoplay?: boolean;
+    autoplaySpeed?: number;
+    beforeChange?: (currentSlide: number, nextSlide: number) => void;
+    centerMode?: boolean;
+    centerPadding?: string;
+    className?: string;
+    cssEase?: string;
+    customPaging?: (index: number) => React.ReactNode;
+    dots?: boolean;
+    dotsClass?: string;
+    draggable?: boolean;
+    easing?: string;
+    fade?: boolean;
+    focusOnSelect?: boolean;
+    infinite?: boolean;
+    initialSlide?: number;
+    lazyLoad?: 'ondemand' | 'progressive';
+    nextArrow?: React.ReactNode;
+    onEdge?: (swipeDirection: 'left' | 'right') => void;
+    onInit?: () => void;
+    onLazyLoad?: (slidesToLoad: number[]) => void;
+    onReInit?: () => void;
+    onSwipe?: (swipeDirection: 'left' | 'right') => void;
+    pauseOnDotsHover?: boolean;
+    pauseOnFocus?: boolean;
+    pauseOnHover?: boolean;
+    prevArrow?: React.ReactNode;
+    responsive?: ResponsiveObject[];
+    rows?: number;
+    rtl?: boolean;
+    slide?: string;
+    slidesPerRow?: number;
+    slidesToScroll?: number;
+    slidesToShow?: number;
+    speed?: number;
+    swipe?: boolean;
+    swipeEvent?: (swipeDirection: 'left' | 'right') => void;
+    swipeToSlide?: boolean;
+    touchMove?: boolean;
+    touchThreshold?: number;
+    useCSS?: boolean;
+    useTransform?: boolean;
+    variableWidth?: boolean;
+    vertical?: boolean;
+    verticalSwiping?: boolean;
+    waitForAnimate?: boolean;
   }
-  
+
+  interface ResponsiveObject {
+    breakpoint: number;
+    settings: 'unslick' | Settings;
+  }
+
+  const Slider: ComponentType<Settings>;
+  export default Slider;
+}
